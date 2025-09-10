471faad18fbaec549c9cf83851402e2f9d4f58d0
parmjeet <iamparmjeetmishra@gmail.com>
2025-09-10 18:15:22 +0530

    Add=>utils index for organization

---
+++ be/src/utils/index.ts
@@ -0,0 +1,6 @@
+export { getReasonPhrase, StatusCode, StatusPhrase } from "./http";
+export { default as serveEmojiFavicon } from "./serve-emoji-favicon";
+export {
+  ZOD_ERROR_CODES,
+  ZOD_ERROR_MESSAGES,
+} from "./validation";

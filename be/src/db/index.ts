53f4ae034f4af964433530d8829136cbfd00dbc8
parmjeet <iamparmjeetmishra@gmail.com>
2025-09-10 18:06:44 +0530

    Add=>db index setup

---
+++ be/src/db/index.ts
@@ -0,0 +1,11 @@
+import { drizzle } from "drizzle-orm/d1";
+import * as schema from "@/db/schema";
+import type { Environment } from "@/env";
+
+export function createDB(env: Environment) {
+  const db = drizzle(env.CF_D1_DB_ID, { schema, casing: "snake_case" });
+  // console.log("db", db);
+  return { db };
+}
+
+export type Database = ReturnType<typeof createDB>["db"];

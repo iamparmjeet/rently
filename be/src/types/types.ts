2465e8880aba7b4aeb7f17096ea807fd7ec0353e
parmjeet <iamparmjeetmishra@gmail.com>
2025-09-11 17:44:12 +0530

    Update=>Auth types added

--- be/src/types/types.ts
+++ be/src/types/types.ts
@@ -1,14 +1,14 @@
+import type { Session, User } from "better-auth";
 import type { PinoLogger } from "hono-pino";
-import z from "zod";
+import type { Database } from "@/db";
 import type { Environment } from "@/env";
 
 export interface AppBindings {
   Bindings: Environment;
   Variables: {
+    database: Database;
     logger: PinoLogger;
+    user?: User;
+    session: Session;
   };
 }
-
-export const IdParamsSchema = z.object({
-  id: z.coerce.number(),
-});

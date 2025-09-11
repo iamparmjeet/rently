af873c4cd6a00d0dd203c6c80eeef150b4397430
parmjeet <iamparmjeetmishra@gmail.com>
2025-09-11 17:47:18 +0530

    Update=>Auth middleware robustness

--- be/src/middleware/auth-middleware.ts
+++ be/src/middleware/auth-middleware.ts
@@ -1,10 +1,34 @@
 import type { Context, Next } from "hono";
 import auth from "@/lib/auth-cli";
+import type { AppBindings } from "@/types/types";
+import { StatusCode, StatusPhrase } from "@/utils";
 
-const withAuth = () => async (c: Context, next: Next) => {
-  const session = await auth.api.getSession(c.req.raw);
-  c.set("session", session);
-  await next();
+const withAuth = () => async (c: Context<AppBindings>, next: Next) => {
+  try {
+    const result = await auth.api.getSession({ headers: c.req.raw.headers });
+
+    if (!result || !result.user || !result.session) {
+      return c.json(
+        {
+          error: StatusPhrase.UNAUTHORIZED,
+        },
+        StatusCode.UNAUTHORIZED
+      );
+    }
+
+    c.set("user", result.user);
+    c.set("session", result.session);
+
+    return next();
+  } catch (error) {
+    console.error("Auth Error", error);
+    return c.json(
+      {
+        error: StatusPhrase.UNAUTHORIZED,
+      },
+      StatusCode.UNAUTHORIZED
+    );
+  }
 };
 
 export default withAuth;

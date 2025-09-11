1f65a99fd93eeda2cda52f790a459cc8f68e6a02
parmjeet <iamparmjeetmishra@gmail.com>
2025-09-11 17:46:49 +0530

    Update=>Auth update for middleware and trycatch

--- be/src/lib/create-app.ts
+++ be/src/lib/create-app.ts
@@ -3,6 +3,7 @@ import { parseEnv } from "@/env";
 import { notFound, onError, pinoLogger } from "@/middleware";
 import type { AppBindings } from "@/types/types";
 import { serveEmojiFavicon } from "@/utils";
+import auth from "./auth-cli";
 
 export function createRouter() {
   return new Hono<AppBindings>({
@@ -12,6 +13,7 @@ export function createRouter() {
 
 export default function createApp() {
   const app = createRouter();
+  app.on(["POST", "GET"], "/api/auth/*", (c) => auth.handler(c.req.raw));
   app.use((c, next) => {
     c.env = parseEnv(Object.assign(c.env || {}, process.env));
     return next();

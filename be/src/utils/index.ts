ee23f57b118b3170ee7faaf5d1614aeb1e912008
parmjeet <iamparmjeetmishra@gmail.com>
2025-09-13 01:31:07 +0530

    Update=>Other exports added

--- be/src/utils/index.ts
+++ be/src/utils/index.ts
@@ -1,5 +1,14 @@
 export { getReasonPhrase, StatusCode, StatusPhrase } from "./http";
+export {
+  badRequest,
+  created,
+  forbidden,
+  sendError,
+  success,
+  unauthorized,
+} from "./responses";
 export { default as serveEmojiFavicon } from "./serve-emoji-favicon";
+export { generateUUID } from "./uuid";
 export {
   ZOD_ERROR_CODES,
   ZOD_ERROR_MESSAGES,

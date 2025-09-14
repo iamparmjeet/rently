150a3c7021e8e1b7f5c0c724dcc39dfd6a537929
parmjeet <iamparmjeetmishra@gmail.com>
2025-09-14 17:53:28 +0530

    Update=>Dates function for DRY

---
+++ be/src/utils/dates.ts
@@ -0,0 +1,9 @@
+export function now(): Date {
+  return new Date();
+}
+
+export function addDays(date: Date, days: number): Date {
+  const result = new Date(date);
+  result.setDate(result.getDate() + days);
+  return result;
+}

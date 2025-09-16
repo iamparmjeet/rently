069ea5a42974551971ff9e4bfdebbeef778b84ea
parmjeet <iamparmjeetmishra@gmail.com>
2025-09-17 01:33:10 +0530

    Update=>db config updated for pg

--- be/drizzle.config.ts
+++ be/drizzle.config.ts
@@ -1,16 +1,13 @@
 import { defineConfig } from "drizzle-kit";
-import { parseEnv } from "@/env";
-
-const env = parseEnv(process.env);
+import { DB_URL } from "@/constants/db-const";
 
 export default defineConfig({
   schema: "./src/db/schema/*.ts",
   out: "./src/db/migrations",
-  dialect: "sqlite",
-  driver: "d1-http",
+  dialect: "postgresql",
   dbCredentials: {
-    accountId: env.CF_ACCOUNT_ID,
-    databaseId: env.CF_D1_DB_ID,
-    token: env.CF_TOKEN_ID,
+    url: DB_URL,
   },
+  casing: "snake_case",
+  verbose: true,
 });

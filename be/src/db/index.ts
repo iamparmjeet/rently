34625e246a34b6f116e884f879bb6e68c8a809bc
parmjeet <iamparmjeetmishra@gmail.com>
2025-09-17 01:32:54 +0530

    Update=>db config updated for pg

--- be/src/db/index.ts
+++ be/src/db/index.ts
@@ -1,11 +1,14 @@
-import { drizzle } from "drizzle-orm/d1";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import { DB_URL } from "@/constants/db-const";
 import * as schema from "@/db/schema";
-import type { Environment } from "@/env";
 
-export function createDB(env: Environment) {
-  const db = drizzle(env.CF_D1_DB_ID, { schema, casing: "snake_case" });
-  // console.log("db", db);
-  return { db };
-}
+// import type { Environment } from "@/env";
 
-export type Database = ReturnType<typeof createDB>["db"];
+const pool = new Pool({
+  connectionString: DB_URL,
+});
+
+export const db = drizzle(pool, { schema, casing: "snake_case" });
+
+export type Database = typeof db;

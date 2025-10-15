98f39989f60ad5a82c5a94b8343913b2d26dd5c8
parmjeet <iamparmjeetmishra@gmail.com>
2025-10-15 17:54:04 +0530

    Updated=>db context added

--- be/src/types/types.ts
+++ be/src/types/types.ts
@@ -1,7 +1,8 @@
 import type { Session, User } from "better-auth";
+import type { Context } from "hono";
 import type { PinoLogger } from "hono-pino";
 import type { UserRole } from "@/constants/user-roles";
-import type { Database } from "@/db";
+import type { db } from "@/db";
 import type { Environment } from "@/env";
 
 export interface ExtendedUser extends User {
@@ -11,9 +12,11 @@ export interface ExtendedUser extends User {
 export interface AppBindings {
   Bindings: Environment;
   Variables: {
-    db: Database;
+    db: typeof db;
     logger: PinoLogger;
-    user?: ExtendedUser;
+    user: ExtendedUser;
     session: Session;
   };
 }
+
+export type Ctx = Context<AppBindings>;

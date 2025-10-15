101f554717f2fedec1dd815044be1a159b46636e
parmjeet <iamparmjeetmishra@gmail.com>
2025-10-15 22:29:37 +0530

    Updated=>Subscription routes added to main app file

--- be/src/app.ts
+++ be/src/app.ts
@@ -1,10 +1,11 @@
 import createApp from "@/lib/create-app";
 import index from "@/routes/index.routes";
 import rentRoutes from "@/routes/rent/routes";
+import subscriptionRoutes from "@/routes/subscriptions/routes";
 
 const app = createApp();
 
-const routes = [index, rentRoutes];
+const routes = [index, rentRoutes, subscriptionRoutes];
 routes.forEach((route) => {
   app.route("/api/v1", route);
 });

095a5706ffa79cc83f092eff039e7627e202da26
parmjeet <iamparmjeetmishra@gmail.com>
2025-10-15 17:53:35 +0530

    Updated=>db middleware added for index

--- be/src/middleware/index.ts
+++ be/src/middleware/index.ts
@@ -1,4 +1,5 @@
 export { default as "auth-middleware" } from "./auth-middleware";
+export { DbBindings, default as dbMiddleware } from "./db-middleware";
 export { default as notFound } from "./not-found";
 export { default as onError } from "./on-error";
 export { default as pinoLogger } from "./pino-logger";

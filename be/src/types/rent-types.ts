f928a8389fd70294580cda70ba514619d00244c2
parmjeet <iamparmjeetmishra@gmail.com>
2025-10-16 14:19:53 +0530

    Updated=>New types added

--- be/src/types/rent-types.ts
+++ be/src/types/rent-types.ts
@@ -12,8 +12,8 @@ import {
 // ******** Property **********
 
 export const PropertySchema = z.object({
-  id: z.uuidv7(),
-  ownerId: z.uuidv7(),
+  id: z.uuid(),
+  ownerId: z.uuid(),
   name: z.string().min(1, "Name is required"),
   address: z.string().min(5, "Address must be at least 5 characters long"),
   type: z.enum(PROPERTY_TYPES_VALUES),
@@ -36,9 +36,9 @@ export const UpdatePropertySchema = PropertySchema.partial().pick({
 // ********* Property ends ************
 
 export const UnitSchema = z.object({
-  id: z.uuidv7(),
-  propertyId: z.uuidv7(),
-  unitNumber: z.number().min(1, "Unit number must be at least 1"),
+  id: z.uuid(),
+  propertyId: z.uuid(),
+  unitNumber: z.string().min(1, "Unit number must be at least 1"),
   type: z.enum(UNIT_TYPES_VALUES),
   area: z.number().min(1, "Area must be at least 1"),
   baseRent: z.number().min(500, "Base Rent must be at least 500"),
@@ -65,15 +65,15 @@ export const UpdateUnitSchema = UnitSchema.partial().pick({
 
 export const LeaseSchema = z
   .object({
-    id: z.uuidv7(),
-    tenantId: z.uuidv7(),
-    unitId: z.number().min(1, "Unit ID must be at least 1"),
+    id: z.uuid(),
+    tenantId: z.uuid(),
+    unitId: z.string().min(1, "Unit ID must be at least 1"),
     startDate: z.date(),
     endDate: z.date().optional(),
     rent: z.number().positive("Rent must be a positive number"),
     deposit: z.number().optional(),
     status: z.enum(LEASE_STATUS_VALUES),
-    referenceId: z.uuidv7(),
+    referenceId: z.uuid(),
     createdAt: z.date(),
     updatedAt: z.date(),
   })
@@ -105,8 +105,8 @@ export const UpdateLeaseSchema = LeaseSchema.partial().pick({
 });
 
 export const UtilitySchema = z.object({
-  id: z.uuidv7(),
-  leaseId: z.uuidv7(),
+  id: z.uuid(),
+  leaseId: z.uuid(),
   utilityType: z.enum(UTILITY_TYPE_VALUES),
   readingDate: z.date(),
   ratePerUnit: z.number().min(1, "Rate per unit must be at least 1").default(9),
@@ -143,13 +143,13 @@ export const UpdateUtilitySchema = UtilitySchema.partial().pick({
 });
 
 export const PaymentSchema = z.object({
-  id: z.uuidv7(),
-  leaseId: z.uuidv7(),
+  id: z.uuid(),
+  leaseId: z.uuid(),
   amount: z.number().positive(),
   paymentDate: z.number(),
   type: z.enum(PAYMENT_TYPE_VALUES),
   description: z.string().optional().nullable(),
-  utilityId: z.uuidv7().optional().nullable(),
+  utilityId: z.uuid().optional().nullable(),
   createdAt: z.date(),
   updatedAt: z.date(),
 });
@@ -170,8 +170,8 @@ export const UpdatePaymentSchema = PaymentSchema.partial().pick({
 });
 
 export const ReferrerSchema = z.object({
-  id: z.uuidv7(),
-  referredUserId: z.uuidv7(),
+  id: z.uuid(),
+  referredUserId: z.uuid(),
   note: z.string().optional(),
   createdAt: z.date(),
   updatedAt: z.date(),
@@ -189,10 +189,10 @@ export const UpdateReferrerSchema = ReferrerSchema.partial().pick({
 });
 
 export const TenantInviteSchema = z.object({
-  id: z.uuidv7(),
+  id: z.uuid(),
   email: z.email(),
-  token: z.uuidv7(),
-  invitedById: z.uuidv7(),
+  token: z.uuid(),
+  invitedById: z.uuid(),
   expiresAt: z.date(),
   status: z.enum(INVITE_STATUS_VALUES).default("pending"),
   createdAt: z.date(),

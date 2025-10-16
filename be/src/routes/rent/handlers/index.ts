9e15ee4c7e893c0739a17024753a12bb9edeee43
parmjeet <iamparmjeetmishra@gmail.com>
2025-10-16 19:23:28 +0530

    Updated=>Utility handler added

--- be/src/routes/rent/handlers/index.ts
+++ be/src/routes/rent/handlers/index.ts
@@ -2,3 +2,4 @@ export * as inviteHandlers from "./invite.handler";
 export * as leaseHandlers from "./lease.handler";
 export * as propertyHandlers from "./property.handler";
 export * as unitHandlers from "./unit.handler";
+export * as utilityHandlers from "./utility.handler";

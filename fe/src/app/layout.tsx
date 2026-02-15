e8b2e02385278d57151efcb586ef4495acadf1fa
iamparmjeet <iamparmjeetmishra@gmail.com>
2026-02-15 23:28:22 +0530

    Update=>Header footer added + Fix container issue

--- fe/src/app/layout.tsx
+++ fe/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/footer/footer";
 import Header from "@/components/header/header";
 
 const geistSans = Geist({
@@ -26,10 +27,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased container mx-auto`}
       >
-        <Header />
-        {children}
+        <main className="flex flex-col min-h-screen">
+          <Header />
+          {children}
+        </main>
+        <Footer />
       </body>
     </html>
   );

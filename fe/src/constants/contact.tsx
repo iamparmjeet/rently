8c6be7c0123446ff7b1d7add01a387be68657fb6
iamparmjeet <iamparmjeetmishra@gmail.com>
2026-02-16 11:26:19 +0530

    Update=>Constant contact seperated

---
+++ fe/src/constants/contact.tsx
@@ -0,0 +1,19 @@
+import { Mail, MapIcon, PhoneCallIcon } from "lucide-react";
+
+export const CONTACT = [
+  {
+    number: "+91 123 123 1234",
+    icon: <PhoneCallIcon />,
+    link: +911231231234,
+  },
+  {
+    email: "info@parmjeetmishra.com",
+    icon: <Mail />,
+    link: "info@parmjeetmishra.com",
+  },
+  {
+    location: "Ludhiana, Punjab",
+    icon: <MapIcon />,
+    link: "location",
+  },
+];

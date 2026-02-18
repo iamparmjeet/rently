931a55776335a93b9a77ca6d9c23564870294eea
iamparmjeet <iamparmjeetmishra@gmail.com>
2026-02-18 23:58:35 +0530

    Update=>Contact Static Data Seperated

--- fe/src/constants/contact.tsx
+++ fe/src/constants/contact.tsx
@@ -1,18 +1,28 @@
 import { Mail, MapIcon, PhoneCallIcon } from "lucide-react";
 
-export const CONTACT = [
+type ContactProp = {
+  key: number;
+  title: string;
+  icon: React.ReactHTMLElement;
+  link: HTMLAnchorElement;
+};
+
+export const CONTACT: ContactProp[] = [
   {
-    number: "+91 123 123 1234",
+    key: 1,
+    title: "+91 123 123 1234",
     icon: <PhoneCallIcon />,
     link: +911231231234,
   },
   {
-    email: "info@parmjeetmishra.com",
+    key: 2,
+    title: "info@parmjeetmishra.com",
     icon: <Mail />,
     link: "info@parmjeetmishra.com",
   },
   {
-    location: "Ludhiana, Punjab",
+    key: 3,
+    title: "Ludhiana, Punjab",
     icon: <MapIcon />,
     link: "location",
   },

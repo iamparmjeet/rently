ecbc7bad0beed279f9330c42a17cef8010cfa297
iamparmjeet <iamparmjeetmishra@gmail.com>
2026-02-18 23:58:04 +0530

    Update=>removed health status

--- fe/src/app/page.tsx
+++ fe/src/app/page.tsx
@@ -1,24 +1,7 @@
-"use client";
-import { useEffect } from "react";
 
-export default function HealthTest() {
-  useEffect(() => {
-    (async () => {
-      try {
-        const res = await fetch("http://localhost:8787/api/v1/health", {
-          credentials: "include",
-        });
-        const data = await res.json();
-        console.log("✅ Health:", data);
-      } catch (err) {
-        console.error("❌ Error:", err);
-      }
-    })();
-  }, []);
 
+export default function Page() {
   return (
-    <div className="p-4">
-      <h1>Check console for backend health response</h1>
-    </div>
-  );
+  <h2>Home Page</h2>
+  )
 }

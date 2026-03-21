a77a696d77ca305f36a202d31e5a7b6b6ce59610
iamparmjeet <iamparmjeetmishra@gmail.com>
2026-03-21 19:32:27 +0530

    Update=>DB updated

--- be/src/constants/db-const.ts
+++ be/src/constants/db-const.ts
@@ -1,3 +1,7 @@
 import env from "@/env";
 
-export const DB_URL = `postgresql://${env.DB_USER}:${env.DB_PASSWORD}@localhost:5432/${env.DB_NAME}`;
+export const DB_URL =
+	env.DB_URL ??
+	`postgresql://${env.DB_USER}:${env.DB_PASSWORD}@${env.DB_HOST}:${env.DB_PORT}/${env.DB_NAME}`;
+
+export const DB_SSL = env.NODE_ENV === "production";

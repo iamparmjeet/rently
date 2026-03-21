fa5aa7f4bcaa6d4ca6d21f16399daaa196cafff6
iamparmjeet <iamparmjeetmishra@gmail.com>
2026-03-21 19:46:20 +0530

    Update=>drizzle update for relative path

--- be/drizzle.config.ts
+++ be/drizzle.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "drizzle-kit";
-import { DB_URL } from "@/constants/db-const";
+import { DB_URL } from "./src/constants/db-const";
+// Here relative path because drizzle run before typescript
 
 export default defineConfig({
   schema: "./src/db/schema/*.ts",

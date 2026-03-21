fe66feb4fdee5f59184ad4a287a271bd881401c0
iamparmjeet <iamparmjeetmishra@gmail.com>
2026-03-21 19:31:38 +0530

    Update=>DB updated

--- be/src/db/index.ts
+++ be/src/db/index.ts
@@ -2,11 +2,20 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import { DB_URL } from "@/constants/db-const";
 import * as schema from "@/db/schema";
+import env from "@/env";
 
 // import type { Environment } from "@/env";
 
 const pool = new Pool({
-  connectionString: DB_URL,
+	connectionString: DB_URL,
+	ssl: env.NODE_ENV === "production" ? { rejectUnauthorized: true } : false,
+	max: 10,
+	idleTimeoutMillis: 3000,
+	connectionTimeoutMillis: 5000,
+});
+
+pool.on("error", (err) => {
+	console.error("Unexpected DB Pool Error", err);
 });
 
 export const db = drizzle(pool, { schema, casing: "snake_case" });

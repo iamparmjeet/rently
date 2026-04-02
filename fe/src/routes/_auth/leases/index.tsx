a65c6aba3821f858b6fa1c03101bde419a1c651d
iamparmjeet <iamparmjeetmishra@gmail.com>
2026-04-02 14:57:44 +0530

    Update=>Remove Tanstack Start

--- fe/src/routes/_auth/leases/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/_auth/leases/")({
-	component: RouteComponent,
-});
-
-function RouteComponent() {
-	return <div>Hello "/api/_auth/leases/"!</div>;
-}

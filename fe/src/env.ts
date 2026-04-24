db28588edba3eebfcc080c002b8197639d3b3181
parmjeet mishra <iamparmjeetmishra@gmail.com>
2026-04-25 00:55:23 +0530

    Updated=>env for local app url

--- fe/src/env.ts
+++ fe/src/env.ts
@@ -2,15 +2,16 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import * as z from "zod";
 
 export const env = createEnv({
-  server: {
-    BETTER_AUTH_URL: z.url(),
-  },
-  client: {
-    NEXT_PUBLIC_BETTER_AUTH_URL: z.url(),
-  },
-  runtimeEnv: {
+	server: {
+		BETTER_AUTH_URL: z.url(),
+	},
+	client: {
+		NEXT_PUBLIC_BETTER_AUTH_URL: z.url(),
+		NEXT_PUBLIC_APP_URL: z.url(),
+	},
+	runtimeEnv: {
 		BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
 		NEXT_PUBLIC_BETTER_AUTH_URL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
-  },
-
+		NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+	},
 });

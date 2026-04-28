aa9b5426a120cf8018348d370d8d37694e3b8cca
parmjeet mishra <iamparmjeetmishra@gmail.com>
2026-04-28 16:43:11 +0530

    Updated=>NODE env and api url

--- fe/src/env.ts
+++ fe/src/env.ts
@@ -4,14 +4,20 @@ import * as z from "zod";
 export const env = createEnv({
 	server: {
 		BETTER_AUTH_URL: z.url(),
+		NODE_ENV: z
+			.enum(["development", "test", "production"])
+			.default("development"),
 	},
 	client: {
 		NEXT_PUBLIC_BETTER_AUTH_URL: z.url(),
 		NEXT_PUBLIC_APP_URL: z.url(),
+		NEXT_PUBLIC_API_URL: z.url(),
 	},
 	runtimeEnv: {
+		NODE_ENV: process.env.NODE_ENV,
 		BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
 		NEXT_PUBLIC_BETTER_AUTH_URL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
 		NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+		NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
 	},
 });

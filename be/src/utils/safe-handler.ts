ee5a8fa2393782bafd182b3c518fb49289d84f1f
parmjeet mishra <iamparmjeetmishra@gmail.com>
2026-04-30 17:23:07 +0530

    Updated=>Safe potential error fix

--- be/src/utils/safe-handler.ts
+++ be/src/utils/safe-handler.ts
@@ -1,17 +1,14 @@
-import type { Ctx } from "@/types/types";
+import type { AppBindings } from "@/types/types";
 import { StatusCode, safeError, sendError } from "@/utils";
+import type { Context } from "hono";
 
-export const safeHandler =
-  (fn: (c: Ctx) => Promise<Response>) => async (c: Ctx) => {
+export const safeHandler = <C extends Context<AppBindings>, R extends Response>
+  (fn: (c: C) => Promise<R>) => async (c: C): Promise<R | Response> => {
     try {
       return await fn(c);
     } catch (err) {
       console.error("Handler Error", safeError(err));
-      return sendError(
-        c,
-        "Internal Server Error",
-        StatusCode.INTERNAL_SERVER_ERROR
-      );
+      return sendError(c, "Internal Server Error", StatusCode.INTERNAL_SERVER_ERROR)
     }
   };
 

5cda8ec540efcbdbdaf8acb989069d9250548704
parmjeet mishra <iamparmjeetmishra@gmail.com>
2026-05-07 23:47:20 +0530

    Updated=>Unused REST handler removed

--- packages/api/src/utils/index.ts
+++ packages/api/src/utils/index.ts
@@ -1,17 +1,5 @@
 export { addDays, now } from "./dates";
 export { getReasonPhrase, StatusCode, StatusPhrase } from "./http";
-export {
-	badRequest,
-	created,
-	forbidden,
-	noContent,
-	notFound,
-	safeError,
-	sendError,
-	success,
-	unauthorized,
-} from "./responses";
-export { default as safeHandler } from "./safe-handler";
 export { default as safeJson } from "./safe-json";
 
 export { generateUUID } from "./uuid";

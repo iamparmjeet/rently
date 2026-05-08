e113a88b541280bf0e771ecb830290e829cacf24
parmjeet mishra <iamparmjeetmishra@gmail.com>
2026-05-08 16:43:06 +0530

    Updated=>single source of validations layer

--- packages/validators/src/property.ts
+++ packages/validators/src/property.ts
@@ -1,18 +1,28 @@
 // ******** Property **********
 
-import { PropertyInsertSchema } from "@rently/db/types";
+import { properties } from "@rently/db/schema/schema";
 
-export const PropertySchema = PropertyInsertSchema;
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import type z from "zod";
 
-export const CreatePropertySchema = PropertySchema.omit({
+// Derive Zod Schemas - For Runtime
+export const PropertySelectSchema = createSelectSchema(properties);
+export const PropertyInsertSchema = createInsertSchema(properties);
+
+// Business Logic Schemas
+export const CreatePropertySchema = PropertyInsertSchema.omit({
 	id: true,
 	ownerId: true,
 	createdAt: true,
 	updatedAt: true,
 });
 
-export const UpdatePropertySchema = PropertySchema.partial().pick({
+export const UpdatePropertySchema = PropertySelectSchema.partial().pick({
 	name: true,
 	address: true,
 	type: true,
 });
+
+// TS Types derieved from Zod (not from InferSelectModel)
+export type Property = z.infer<typeof PropertySelectSchema>;
+export type NewProperty = z.infer<typeof PropertyInsertSchema>;

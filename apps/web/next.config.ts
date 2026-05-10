78cb76ebb3e68b212b26f6bb85c742f76a790ae7
parmjeet mishra <iamparmjeetmishra@gmail.com>
2026-05-11 00:12:15 +0530

    Updated=>Nextjs image remote pattersn added

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -4,6 +4,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	typedRoutes: true,
 	reactCompiler: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "lh3.googleusercontent.com",
+			},
+			{
+				protocol: "https",
+				hostname: "github.com",
+			},
+		],
+	},
 };
 
 export default nextConfig;

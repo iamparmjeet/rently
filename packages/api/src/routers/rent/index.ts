cc7304a899ffba22401966113eec869916479ecf
parmjeet mishra <iamparmjeetmishra@gmail.com>
2026-05-12 15:18:00 +0530

    Updated=>Unit Route added

--- packages/api/src/routers/rent/index.ts
+++ packages/api/src/routers/rent/index.ts
@@ -1,5 +1,7 @@
 import * as propertyProcedures from "./property";
+import * as unitProcedures from "./unit";
 
 export const rentRouter = {
 	property: propertyProcedures,
+	unit: unitProcedures,
 };

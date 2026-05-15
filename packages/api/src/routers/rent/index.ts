ff6d8bf28b8f001b598fc59128bfaaa4ce34944b
parmjeet mishra <iamparmjeetmishra@gmail.com>
2026-05-15 15:41:50 +0530

    Update=>lease and tenant procedures added

--- packages/api/src/routers/rent/index.ts
+++ packages/api/src/routers/rent/index.ts
@@ -1,7 +1,11 @@
+import * as leaseProcedures from "./lease";
 import * as propertyProcedures from "./property";
+import * as tenantProcedures from "./tenant";
 import * as unitProcedures from "./unit";
 
 export const rentRouter = {
 	property: propertyProcedures,
 	unit: unitProcedures,
+	lease: leaseProcedures,
+	tenant: tenantProcedures,
 };

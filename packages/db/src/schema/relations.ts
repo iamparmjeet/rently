514ce559483330223a171b69ce4ff5d9bf1341bd
parmjeet mishra <iamparmjeetmishra@gmail.com>
2026-05-16 23:27:14 +0530

    Update=>relations linked with user = owner

--- packages/db/src/schema/relations.ts
+++ packages/db/src/schema/relations.ts
@@ -1,3 +1,4 @@
+import { relations } from "drizzle-orm";
 import {
 	invoices,
 	leases,
@@ -11,8 +12,7 @@ import {
 	units,
 	user,
 	utilities,
-} from "@rently/db/schema";
-import { relations } from "drizzle-orm";
+} from "./index";
 
 // 1) Property ↔ Owner (user)
 export const userRelations = relations(user, ({ many }) => ({

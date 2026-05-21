07bc447f879932d22dcf9f8ae15a679a9cbbb857
parmjeet mishra <iamparmjeetmishra@gmail.com>
2026-05-21 22:42:40 +0530

    Update=>payment utility procedures added

--- packages/api/src/routers/rent/index.ts
+++ packages/api/src/routers/rent/index.ts
@@ -1,8 +1,10 @@
 import * as inviteProcedures from "./invite";
 import * as leaseProcedures from "./lease";
+import * as paymentProcedures from "./payment";
 import * as propertyProcedures from "./property";
 import * as tenantProcedures from "./tenant";
 import * as unitProcedures from "./unit";
+import * as utilityProcedures from "./utility";
 
 export const rentRouter = {
 	property: propertyProcedures,
@@ -10,4 +12,6 @@ export const rentRouter = {
 	lease: leaseProcedures,
 	tenant: tenantProcedures,
 	invite: inviteProcedures,
+	payment: paymentProcedures,
+	utility: utilityProcedures,
 };

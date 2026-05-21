7ce2789ba136ccb9d08258965b25b5c5f1dbe513
parmjeet mishra <iamparmjeetmishra@gmail.com>
2026-05-21 22:21:13 +0530

    Update=>Property validators layer with single source of truth db

--- packages/validators/src/property.ts
+++ packages/validators/src/property.ts
@@ -1,13 +1,18 @@
 import { properties } from "@rently/db/schema/schema";
-
-import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import {
+	createInsertSchema,
+	createSelectSchema,
+	createUpdateSchema,
+} from "drizzle-zod";
 import type z from "zod";
 
 // ******** Property **********
+// Layer 1 ) DB Derived
 // Derive Zod Schemas - For Runtime
 export const PropertySelectSchema = createSelectSchema(properties);
 export const PropertyInsertSchema = createInsertSchema(properties);
 
+//  Layer 2 ) - API Input schemas
 // Business Logic Schemas
 export const CreatePropertySchema = PropertyInsertSchema.omit({
 	id: true,
@@ -16,12 +21,26 @@ export const CreatePropertySchema = PropertyInsertSchema.omit({
 	updatedAt: true,
 });
 
-export const UpdatePropertySchema = PropertySelectSchema.partial().pick({
+export const UpdatePropertySchema = createUpdateSchema(properties).pick({
 	name: true,
 	address: true,
 	type: true,
 });
 
+// Layer 3 : API Output Schemas
+// What the API Returns
+export const PropertyPublicSchema = PropertySelectSchema;
+export const PropertyListItemSchema = PropertySelectSchema.pick({
+	id: true,
+	name: true,
+	address: true,
+	type: true,
+	createdAt: true,
+});
+
 // TS Types derieved from Zod (not from InferSelectModel)
 export type Property = z.infer<typeof PropertySelectSchema>;
 export type NewProperty = z.infer<typeof PropertyInsertSchema>;
+export type CreateProperty = z.infer<typeof CreatePropertySchema>;
+export type UpdateProperty = z.infer<typeof UpdatePropertySchema>;
+export type PropertyListItem = z.infer<typeof PropertyListItemSchema>;

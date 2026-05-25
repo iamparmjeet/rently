f2aca92e3d4cb1583c422d07b1bac9370b1655cb
Parmjeet Mishra <iamparmjeetmishra@gmail.com>
2026-05-25 13:21:04 +0530

    Update=>suspense query api

--- apps/web/src/hooks/tenants/use-tenants.ts
+++ apps/web/src/hooks/tenants/use-tenants.ts
@@ -1,6 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
 import { orpc } from "@/utils/orpc";
 
 export function useTenants() {
 	return useQuery(orpc.rent.tenant.listTenants.queryOptions());
+}
+
+export function useSuspenseTenants() {
+	return useSuspenseQuery(orpc.rent.tenant.listTenants.queryOptions());
 }

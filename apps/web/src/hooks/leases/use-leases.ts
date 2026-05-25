d61b59904aebdc194742b6340c0ee6b2f25e5ffb
Parmjeet Mishra <iamparmjeetmishra@gmail.com>
2026-05-25 13:21:38 +0530

    Update=>suspense query api

--- apps/web/src/hooks/leases/use-leases.ts
+++ apps/web/src/hooks/leases/use-leases.ts
@@ -1,6 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
 import { orpc } from "@/utils/orpc";
 
 export function useLeases() {
 	return useQuery(orpc.rent.lease.listLeases.queryOptions());
+}
+
+export function useSuspenseLeases() {
+	return useSuspenseQuery(orpc.rent.lease.listLeases.queryOptions());
 }

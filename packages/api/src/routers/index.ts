09cbf929acc4206ff41b2778edf634be77974328
Parmjeet Mishra <iamparmjeetmishra@gmail.com>
2026-05-30 15:19:06 +0530

    Update=>comments removed

--- packages/api/src/routers/index.ts
+++ packages/api/src/routers/index.ts
@@ -2,11 +2,6 @@ import type { RouterClient } from "@orpc/server";
 import { rentRouter } from "./rent";
 
 export const appRouter = {
-	// Debug or remove or moved
-	// privateDate: protectedProcedure.handler(({ context }) => ({
-	// 	message: "This is private",
-	// 	user: context.user,
-	// })),
 	rent: rentRouter,
 };
 

1617c793bc798da418c61f805ea3f92f9b258c7a
Parmjeet Mishra <iamparmjeetmishra@gmail.com>
2026-05-30 15:04:06 +0530

    Update=>orpc provider added

---
+++ apps/web/src/components/providers/orpc-provider.tsx
@@ -0,0 +1,28 @@
+"use client";
+import {
+	createAppClient,
+	createAppUtils,
+	createRPCLink,
+} from "@rently/api/client";
+import { env } from "@rently/env/web";
+import { ORPCProvider } from "@rently/hooks/orpc";
+
+const link = createRPCLink({
+	serverURL: env.NEXT_PUBLIC_SERVER_URL,
+	getHeaders: async () => {
+		if (typeof window !== "undefined") return {};
+		const { headers } = await import("next/headers");
+		return Object.fromEntries(await headers());
+	},
+});
+
+const client = createAppClient(link);
+const utils = createAppUtils(client);
+
+export function WebORPCProvider({ children }: { children: React.ReactNode }) {
+	return (
+		<ORPCProvider client={client} utils={utils}>
+			{children}
+		</ORPCProvider>
+	);
+}

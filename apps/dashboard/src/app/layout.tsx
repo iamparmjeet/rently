0861b5616da17bbc1444590e6c50285e97e11f41
Parmjeet Mishra <iamparmjeetmishra@gmail.com>
2026-05-31 19:31:58 +0530

    Update=>Sidebar and header issue fixed

--- apps/dashboard/src/app/layout.tsx
+++ apps/dashboard/src/app/layout.tsx
@@ -2,8 +2,11 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 
 import "../index.css";
+import { SidebarProvider } from "@rently/ui/components/sidebar";
 import { Toaster } from "@rently/ui/components/sonner";
 import { cn } from "@rently/ui/lib/utils";
+import DashboardHeader from "@/components/layouts/dashboard-header";
+import { DashbaordSidebar } from "@/components/layouts/dashboard-sidebar";
 import { DashboardORPCProvider } from "@/components/providers/orpc-provider";
 import { QueryProvider } from "@/components/providers/query-provider";
 
@@ -40,10 +43,14 @@ export default function RootLayout({
 			>
 				<QueryProvider>
 					<DashboardORPCProvider>
-						<div className="flex min-h-screen flex-col">
-							{children}
-							<Toaster position="bottom-right" />
-						</div>
+						<SidebarProvider>
+							<DashbaordSidebar />
+							<div className="flex min-h-screen w-full flex-col">
+								<DashboardHeader />
+								<main className="bg-mist-100 px-6 py-4">{children}</main>
+								<Toaster position="bottom-right" />
+							</div>
+						</SidebarProvider>
 					</DashboardORPCProvider>
 				</QueryProvider>
 			</body>

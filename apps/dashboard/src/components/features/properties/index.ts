a62cc0dad9363c02156fe04ad85a5f0770d70541
Parmjeet Mishra <iamparmjeetmishra@gmail.com>
2026-06-02 13:23:42 +0530

    refactor: -> index added for single point of import and export

--- apps/dashboard/src/components/features/properties/index.ts
+++ apps/dashboard/src/components/features/properties/index.ts
@@ -1,5 +1,5 @@
 export { PropertyCard } from "./property-card";
 export { PropertyFiltersBar } from "./property-filters";
-// export {type, } from "./property-list"
+export { PropertyGrid } from "./property-grid";
 export { PropertyStats } from "./property-stats";
 export { PropertyTable } from "./property-table";

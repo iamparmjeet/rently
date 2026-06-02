ef634a11601694f6a06ab228f813ec48d492f84b
Parmjeet Mishra <iamparmjeetmishra@gmail.com>
2026-06-02 13:24:40 +0530

    refactor: -> suspense removed due to internal page has loading

--- apps/dashboard/src/app/properties/page.tsx
+++ apps/dashboard/src/app/properties/page.tsx
@@ -1,14 +1,5 @@
-// server component
-// Suspense fallback
-
-import { Suspense } from "react";
 import PropertiesClient from "./_components/properties-client";
-import { PropertiesPageSkeleton } from "./_components/properties-page-skelton";
 
 export default function PropertiesPage() {
-	return (
-		<Suspense fallback={<PropertiesPageSkeleton />}>
-			<PropertiesClient />
-		</Suspense>
-	);
+	return <PropertiesClient />;
 }

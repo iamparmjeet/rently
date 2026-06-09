8ec8b0decdd15b229a4b79d76d8585daff4763ac
Parmjeet Mishra <iamparmjeetmishra@gmail.com>
2026-06-09 15:00:08 +0530

    feat: subscription procedures added

--- packages/api/src/routers/index.ts
+++ packages/api/src/routers/index.ts
@@ -1,8 +1,10 @@
 import type { RouterClient } from "@orpc/server";
 import { rentRouter } from "./rent";
+import { subscriptionProcedures } from "./subscriptions";
 
 export const appRouter = {
 	rent: rentRouter,
+	subscription: subscriptionProcedures,
 };
 
 export type AppRouter = typeof appRouter;

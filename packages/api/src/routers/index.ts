de9427bf2867a7e89596029cd51e04390261dbb3
Parmjeet Mishra <iamparmjeetmishra@gmail.com>
2026-06-09 17:35:23 +0530

    feat: r2 procedures added

--- packages/api/src/routers/index.ts
+++ packages/api/src/routers/index.ts
@@ -1,10 +1,12 @@
 import type { RouterClient } from "@orpc/server";
 import { rentRouter } from "./rent";
 import { subscriptionProcedures } from "./subscriptions";
+import * as uploadProcedures from "./upload";
 
 export const appRouter = {
 	rent: rentRouter,
 	subscription: subscriptionProcedures,
+	upload: uploadProcedures,
 };
 
 export type AppRouter = typeof appRouter;

c73e01a7fb389122092b88bc1059021c43f1173b
Parmjeet Mishra <iamparmjeetmishra@gmail.com>
2026-06-09 14:34:36 +0530

    refactor: subscription added

--- packages/validators/src/index.ts
+++ packages/validators/src/index.ts
@@ -6,6 +6,7 @@ export * from "./lease";
 export * from "./owner-profile";
 export * from "./payment";
 export * from "./property";
+export * from "./subscription";
 export * from "./tenant";
 export * from "./unit";
 export * from "./utility";

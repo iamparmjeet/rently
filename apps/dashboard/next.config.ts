86c9e90bd79f60307712455357a0c9880d6f1c78
Parmjeet Mishra <iamparmjeetmishra@gmail.com>
2026-06-10 23:59:00 +0530

    fix(auth): next config for cors

--- apps/dashboard/next.config.ts
+++ apps/dashboard/next.config.ts
@@ -1,6 +1,11 @@
 import "@rently/env/web";
 import type { NextConfig } from "next";
 
+const allowedDashboardOrigins = [
+	"https://dashboard-rentwise.parmjeetmishra.com",
+	"https://tenant-rentwise.parmjeetmishra.com",
+];
+
 const nextConfig: NextConfig = {
 	typedRoutes: true,
 	reactCompiler: true,
@@ -9,8 +14,34 @@ const nextConfig: NextConfig = {
 		remotePatterns: [
 			{ protocol: "https", hostname: "lh3.googleusercontent.com" },
 			{ protocol: "https", hostname: "github.com" },
-			{ protocol: "https", hostname: "rentwise-media.parmjeetmishra.com" },
 		],
+	},
+	async headers() {
+		return [
+			{
+				source: "/(login|register|forgot-password)(.*)",
+				headers: [
+					{
+						key: "Access-Control-Allow-Origin",
+						// value: allowedDashboardOrigins.join(", "),
+						value: "*",
+					},
+					{
+						key: "Access-Control-Allow-Methods",
+						value: "GET, OPTIONS",
+					},
+					{
+						key: "Access-Control-Allow-Headers",
+						value:
+							"Content-Type, RSC, Next-Router-Prefetch, Next-Router-State-Tree, Next-Url",
+					},
+					{
+						key: "Access-Control-Allow-Credentials",
+						value: "true",
+					},
+				],
+			},
+		];
 	},
 };
 
